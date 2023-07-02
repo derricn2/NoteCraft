676569ffd57ef4ca97ad99199936aa92e6069979
derricn2 <derricn2@gmail.com>
2023-07-02 15:46:52 -0700

    reviewed and troubleshoot issue of not loading notes page

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // serve static files from public folder
-app.use(express.status('public'));
+app.use(express.static('public'));
 
 // two routes; one for homepage, one for notes page, both routes send the corresponding HTML file
 app.get('/', (req, res) => {
-    res.sendFile(--dirname + 'public/index.html');
+    res.sendFile(--dirname + '/public/index.html');
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(__dirname + 'public/notes.html');
+    res.sendFile(__dirname + '/public/notes.html');
 });
 
 // express app listening on port
