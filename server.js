3eaa6f2f9e85f1eb3e2cd48763678ff8efe2b030
derricn2 <derricn2@gmail.com>
2023-07-02 15:29:42 -0700

    added middleware, two routes (for homepage and notes page)

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 // import express module
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5500;
 
+//middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// serve static files from public folder
+app.use(express.status('public'));
+
+// two routes; one for homepage, one for notes page, both routes send the corresponding HTML file
+app.get('/', (req, res) => {
+    res.sendFile(--dirname + 'public/index.html');
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(__dirname + 'public/notes.html');
+});
 
 // express app listening on port
 app.listen(PORT, () => {
