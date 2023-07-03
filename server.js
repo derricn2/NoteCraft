2935bd5d1c6dc3d55dac6e15635d952af83ef736
derricn2 <derricn2@gmail.com>
2023-07-03 15:43:52 -0700

    updated imported modules, optimized apiRoute code, deleted extra js file, added additional HTML route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-// import express module
+// import module
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5500;
 
@@ -10,13 +12,27 @@ app.use(express.urlencoded({ extended: true }));
 // serve static files from public folder
 app.use(express.static('public'));
 
+// Read notes from the db.json file
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      res.json(notes);
+    });
+  });  
+
 // two routes; one for homepage, one for notes page, both routes send the corresponding HTML file
 app.get('/', (req, res) => {
-    res.sendFile(--dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/notes', (req, res) => {
     res.sendFile(__dirname + '/public/notes.html');
+});
+
+// additional HTML route in case of typos
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 // express app listening on port
